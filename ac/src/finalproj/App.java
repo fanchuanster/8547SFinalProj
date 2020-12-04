5d8f1df8b69b6a82d48b6249af6554fdcc708710
Wen Dong <dong23@uwindsor.ca>
2020-12-04 13:35:37 +0800

    test

--- ac/src/finalproj/App.java
+++ ac/src/finalproj/App.java
@@ -2,6 +2,7 @@ package finalproj;
 
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -15,19 +16,29 @@ public class App
 	 * finding keywords using string matching, use of inverted index, analyzing frequencies using hash tables or search trees,
 	 * using large dictionaries/datasets, sorting techniques, search trees, spellchecking keywords or HTML files, and many others.  
 	 */
+	
+	static int searchFile() {
+		return 0;
+	}
 	 
     public static void main( String[] args )
     {
-    	String outputDir = "output/pages/";
+    	final String outputDir = "output/pages/";
     	
-        System.out.println( "Hello World!" );
+    	Scanner sc = new Scanner(System.in);
+        System.out.println( "Hello! Please input a link to search" );
+        String url = sc.nextLine();
+        url = "https://www.newsmax.com/";
         
-        WebCrawler crawler = new WebCrawler("https://www.newsmax.com/", 3, 50, outputDir,
+        WebCrawler crawler = new WebCrawler(url, 3, 50, outputDir,
         		new Proxy(Proxy.Type.HTTP, new InetSocketAddress("web-proxy.il.softwaregrp.net", 8080)));
 //        crawler.trustEveryone();
         
         int count = crawler.downloadPages();
         
         System.out.println(String.format("%d pages downloaded,find them in %s", count, crawler.getPagesDir()));
+        
+        System.out.println( "Hello! Please input a string to search for" );
+        String word = sc.nextLine();
     }
 }
