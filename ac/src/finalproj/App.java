45473f3d17eb9d6d7ac4b4562d2846994d85f454
Wen Dong <wen.dong@microfocus.com>
2020-12-05 21:23:44 +0800

    cleanup legecy

--- ac/src/finalproj/App.java
+++ ac/src/finalproj/App.java
@@ -1,43 +1,36 @@
 package finalproj;
 
-import textprocessing.StdOut;
-import textprocessing.TST;
-import textprocessing.TrieST;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
 
 public class App {
 
 	public static void main(String[] args) {
-		String[] keys = {"she","sells","sea","shells","by","the","sea","shore"}; 
-    	
-    	// build symbol table from standard input
-        TrieST<Integer> st = new TrieST<Integer>();
-        for (int i = 0; i < keys.length; i++) {
-            //String key = In.readString();
-            st.put(keys[i], i);
+//    	Scanner sc = new Scanner(System.in);
+//      System.out.println( "Hello! Please input a link to search" );
+//      String url = sc.nextLine();
+		
+		Hashtable<String, String[]> tosearches = new Hashtable<String, String[]>();
+		tosearches.put("https://www.newsmax.com/", new String[]{"newsmax", "will"});
+		tosearches.put("https://en.wikipedia.org/wiki/New_Tang_Dynasty_Television", new String[]{"television", "group"});
+		tosearches.put("https://www.newsmax.com/", new String[]{"win", "sidney"});
+		
+        WebSearchEngine engine = new WebSearchEngine(3,5);
+        
+        Set<String> urls = tosearches.keySet();
+        for (final String url:urls) {
+        	String[] keywords = tosearches.get(url);
+        	for (final String keyword:keywords) {
+            	List<SearchResult> tops = engine.searchPattern(keyword, url, 3);
+
+                System.out.println(String.format("\n===================Search Result of '%s'====================", keyword));
+                tops.stream().map(s -> String.format("%d times\t%s\n", s.occurrences, s.pageUrl)).forEach(System.out::print);
+        	}
         }
-
-        // print results
-        if (st.size() < 100) {
-            StdOut.println("keys(\"\"):");
-            for (String key : st.keys()) {
-                StdOut.println(key + " " + st.get(key));
-            }
-            StdOut.println();
-        }
-
-        StdOut.println("longestPrefixOf(\"shellsort\"):");
-        StdOut.println(st.longestPrefixOf("shellsort"));
-        StdOut.println();
-
-        StdOut.println("keysWithPrefix(\"shor\"):");
-        for (String s : st.keysWithPrefix("shor"))
-            StdOut.println(s);
-        StdOut.println();
-
-        StdOut.println("keysThatMatch(\"sea\"):");
-        for (String s : st.keysThatMatch("sea"))
-            StdOut.println(s);
-
-	}
+        
+//        System.out.println( "Hello! Please input a string to search for" );
+//        String word = sc.nextLine();	
+    }
 
 }
--- ac/src/finalproj/WebSearchEngine.java
+++ ac/src/finalproj/WebSearchEngine.java
@@ -9,15 +9,12 @@ import java.net.Proxy;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
-import challenge.IntKeyObject;
 import sorting.*;
 import textprocessing.TST;
 import common.Util;
@@ -194,31 +191,4 @@ public class WebSearchEngine {
 		return getTopResults(results, topN);
 	}
 	
-	public static void main(String[] args) {
-		
-//    	Scanner sc = new Scanner(System.in);
-//      System.out.println( "Hello! Please input a link to search" );
-//      String url = sc.nextLine();
-		
-		Hashtable<String, String[]> tosearches = new Hashtable<String, String[]>();
-		tosearches.put("https://www.newsmax.com/", new String[]{"newsmax", "will"});
-		tosearches.put("https://en.wikipedia.org/wiki/New_Tang_Dynasty_Television", new String[]{"television", "group"});
-		tosearches.put("https://www.newsmax.com/", new String[]{"win", "sidney"});
-		
-        WebSearchEngine engine = new WebSearchEngine(3,5);
-        
-        Set<String> urls = tosearches.keySet();
-        for (final String url:urls) {
-        	String[] keywords = tosearches.get(url);
-        	for (final String keyword:keywords) {
-            	List<SearchResult> tops = engine.searchPattern(keyword, url, 3);
-
-                System.out.println(String.format("\n===================Search Result of '%s'====================", keyword));
-                tops.stream().map(s -> String.format("%d\t%s\n", s.occurrences, s.pageUrl)).forEach(System.out::print);
-        	}
-        }
-        
-//        System.out.println( "Hello! Please input a string to search for" );
-//        String word = sc.nextLine();
-	}
 }
